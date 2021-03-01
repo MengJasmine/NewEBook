110f117e577c4419351ee5321ca8944bdc95e30d
ChloeYO <aoyangyou@hotmail.com>
2021-02-28 21:23:56 -0800

    第三章项目准备

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <span class ='text'>abcde</span>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+<script>
+  import { mapGetters } from 'vuex'
+  const getters = {
+    a: () => 1,
+    b: () => 2,
+    c: () => 3
+  }
+  function fn(keys) {
+    const data = {}
+      keys.forEach(key => {
+        if (getters.hasOwnProperty(key)) {
+          data[key] = getters[key]
+        }
+      })
+      return data
+    }
+  export default {
+    computed: {
+      ...mapGetters(['test']),
+      ...fn(['a', 'b', 'c'])
+    },
+    mounted () {
+      //this.$store.dispatch('setTest', 9).then(() => {
+      //console.log(this.test)
+      // })
+      console.log(this.a, this.b, this.c)
+    }
+  }
+  document.addEventListener('DOMContentLoaded', () => {
+  // 获取html元素
+    const html = document.querySelector('html')
+  // 根据屏幕宽度动态计算fontSize
+    let fontSize = window.innerWidth / 10
+  // 当fontSize大于50时，设置最大值为50
+    fontSize = fontSize > 50 ? 50 : fontSize
+  // 设置html的fontSize属性，使rem生效，1rem = fontSize + 'px'
+    html.style.fontSize = fontSize + 'px'
+  })
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style lang="scss" scoped>
+  @import "./assets/styles/global";
+  .text{
+  width: 100%;
+  height: 100%;
+  background: #7d8188;
+  overflow: hidden;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span class = "icon-bookmark"></span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  export default {
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
   }
-}
 </script>
+
+<style scoped>
+  .icon-bookmark{
+    font-size: 30px;
+    color: #ff0000;
+  }
+</style>
