406c999ba2a210a4efed48e4b604e4d2fd65dfaf
ChloeYO <aoyangyou@hotmail.com>
2021-03-19 10:23:19 -0700

    Chapter4-Finished

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,28 @@
 <template>
   <div id="app">
-    <span class ='text'>abcde</span>
     <router-view/>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  const getters = {
-    a: () => 1,
-    b: () => 2,
-    c: () => 3
-  }
-  function fn(keys) {
-    const data = {}
-      keys.forEach(key => {
-        if (getters.hasOwnProperty(key)) {
-          data[key] = getters[key]
-        }
-      })
-      return data
-    }
-  export default {
-    computed: {
-      ...mapGetters(['test']),
-      ...fn(['a', 'b', 'c'])
-    },
-    mounted () {
-      //this.$store.dispatch('setTest', 9).then(() => {
-      //console.log(this.test)
-      // })
-      console.log(this.a, this.b, this.c)
-    }
-  }
+  export default {}
   document.addEventListener('DOMContentLoaded', () => {
   // 获取html元素
     const html = document.querySelector('html')
   // 根据屏幕宽度动态计算fontSize
     let fontSize = window.innerWidth / 10
-  // 当fontSize大于50时，设置最大值为50
+
     fontSize = fontSize > 50 ? 50 : fontSize
   // 设置html的fontSize属性，使rem生效，1rem = fontSize + 'px'
     html.style.fontSize = fontSize + 'px'
-  })
+})
 </script>
 
 <style lang="scss" scoped>
-  @import "./assets/styles/global";
-  .text{
-  width: 100%;
-  height: 100%;
-  background: #7d8188;
-  overflow: hidden;
+  #app {
+    width: 100%;
+    height: 100%;
+    background: #7d8188;
+    overflow: hidden;
 }
 </style>
---
+++ src/views/ebook/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="ebook" ref="ebook">
+    <EbookTitle></EbookTitle>
+    <ebook-reader></ebook-reader>
+    <ebook-menu></ebook-menu>
+  </div>
+</template>
+
+<script>
+  import EbookReader from '../../components/ebook/EbookReader'
+  import EbookTitle from '../../components/ebook/EbookTitle'
+  import EbookMenu from '../../components/ebook/EbookMenu'
+  export default {
+  components: {
+    EbookReader,
+    EbookTitle,
+    EbookMenu
+  }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+
+  .ebook {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
